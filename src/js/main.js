4742239c55a5137e361c0a2f347e966dedd2dcbe
JohnsonBeast7 <joaopedro.ferraresi@gmail.com>
2025-10-01 00:56:59 -0300

    ajustes e js para não mostrar ids na url

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import '../css/app.css';
+
+  const linksInternos = document.querySelectorAll('.link-interno');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection);
+  });
