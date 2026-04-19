743b9515eece826e1182bbe6fd0a16a0d20185c4
JohnsonBeast7 <joaopedro.ferraresi@gmail.com>
2026-04-18 21:34:15 -0300

    Revert "change theme color from indigo to #950e11 and update favicon"
    This reverts commit ed8d9d7533cc222def7031fdf574ef86d42a559e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        brand: {
-          50:  '#fef2f2',
-          500: '#950e11',
-          600: '#7a0b0e',
-        },
-      },
       backgroundImage: {
         'hero': "url('/src/imgs/bg-hero.webp')",
         'servico-1': "url('/src/imgs/servicos-1.webp')",
